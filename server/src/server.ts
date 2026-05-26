f18ebb7dde1c7607797db11c9429cd2924a939c3
yashukaSan <ysahu1590@gmail.com>
2026-05-26 23:50:48 +0530

    update the scripts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-import app from './app';
-import connectDB from './config/db';
+import app from './app.ts';
+import connectDB from './config/db.ts';
 
 const PORT = process.env.PORT ?? 5000;
 
